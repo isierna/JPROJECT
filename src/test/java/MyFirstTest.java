286dfe972881f23e6920bbce5a2238d1673ed474
Ira <ira_www@yahoo.com>
2016-09-28 11:06:12 +0300

    Yahoo login tes with password and phone population issue.

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -2,38 +2,106 @@
  * Created by Ira on 9/20/16.
  */
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
+import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 public class MyFirstTest {
     static WebDriver driver;
 
+    @BeforeMethod
+    public static void openBrowser() {
+        driver = new FirefoxDriver();
+    }
+
+    private static WebElement waitUntil(ExpectedCondition<WebElement> condition) {
+        return (new WebDriverWait(driver, 10)).until(condition);
+    }
+
     @Test
     public static void openGoogle() {
-        driver = new FirefoxDriver();
-
         driver.get("http://google.com");
+        driver.manage().window().maximize();
 
         WebElement element = driver.findElement(By.name("q"));
         element.sendKeys("AfterShot");
         element.submit();
 
-        WebElement dynamicElement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText("RAW")));
+        WebElement dynamicElement = waitUntil(presenceOfElementLocated(By.partialLinkText("RAW")));
         dynamicElement.click();
 
-        WebElement applicationPage = (new WebDriverWait(driver,10)).until(ExpectedConditions.presenceOfElementLocated(By.id("asp-trial-nd")));
+        WebElement applicationPage = waitUntil(presenceOfElementLocated(By.id("asp-trial-nd")));
         System.out.println(applicationPage);
 
         applicationPage.click();
+
+    }
+
+    @Test
+    public static void login() {
+        driver.get("http://yahoo.com");//open the web page
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("uh-signin")).click();  //click at sign in button
+
+        driver.findElement(By.id("login-signup")).click(); //click at signup button
+
+
+
+        WebElement first_name = waitUntil(presenceOfElementLocated(By.id("usernamereg-firstName")));
+
+        WebElement last_name = waitUntil(presenceOfElementLocated(By.id("usernamereg-lastName")));
+
+        WebElement email = waitUntil(presenceOfElementLocated(By.id("usernamereg-yid")));
+
+        WebElement password = waitUntil(presenceOfElementLocated(By.id("usernamereg-password")));
+
+
+
+        String randomString;
+        randomString = RandomStringUtils.randomAlphabetic(8); //generating a random string
+
+        first_name.sendKeys(randomString);
+        last_name.sendKeys(randomString);
+        email.sendKeys(randomString);
+
+        String password1 = RandomStringUtils.randomAlphanumeric(10);
+
+
+        password.clear();
+        password.sendKeys(password1);
+
+        Select country = new Select(waitUntil(presenceOfElementLocated(By.name("shortCountryCode"))));
+        country.selectByValue("MC");
+
+        WebElement phone_number = waitUntil(presenceOfElementLocated(By.id("usernamereg-phone")));
+        Actions action = new Actions(driver);
+        action.moveToElement(email).click();
+        phone_number.clear();
+        phone_number.sendKeys("0961454392");
+
+        Select month = new Select(waitUntil(presenceOfElementLocated(By.id("usernamereg-month"))));
+        month.selectByValue("1");
+
+        Select day = new Select(waitUntil(presenceOfElementLocated(By.id("usernamereg-day"))));
+        day.selectByValue("1");
+
+        Select year = new Select(waitUntil(presenceOfElementLocated(By.id("usernamereg-year"))));
+        year.selectByValue("1990");
+
+        WebElement submit_button = waitUntil(presenceOfElementLocated(By.id("reg-submit-button")));
+        submit_button.click();
 
     }
 
