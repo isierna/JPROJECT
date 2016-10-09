c14cee362c5dd5585b348872044fa404d372dfbb
Ira <ira_www@yahoo.com>
2016-10-09 06:34:52 +0300

    Finished photoGet()

--- src/test/java/PhotoDownload.java
+++ src/test/java/PhotoDownload.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -9,8 +8,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.io.IOException;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
@@ -31,7 +31,6 @@ public class PhotoDownload {
         profile.setPreference("browser.download.folderList", 2);
         profile.setPreference("browser.download.dir", "/Users/Ira/Pictures/1");
         profile.setPreference("browser.helperApps.neverAsk.saveToDisk", "image/x-cr2");
-
         driver = new FirefoxDriver(profile);
     }
 
@@ -42,7 +41,7 @@ public class PhotoDownload {
     private static void checkForAd() {
         try {
             driver.findElement(By.linkText("//*[@id=\"AdFloater\"]/a[1]")).click();
-        }catch (NoSuchElementException e) {
+        } catch (NoSuchElementException e) {
             System.out.println("Error " + e);
         }
     }
@@ -50,6 +49,14 @@ public class PhotoDownload {
 
     @Test
     public static void photoGet() throws IOException, AWTException {
+        String main_directory = "/Users/Ira/Pictures/1/" + maker + "_" + model;
+        String jpg_directory = main_directory + "/JPG";
+        String raw_directory = main_directory + "/RAW";
+
+        new File(main_directory).mkdir();
+        new File(jpg_directory).mkdir();
+        new File(raw_directory).mkdir();
+
         driver.get("http://www.imaging-resource.com/cameras/reviews/"); //open driver and main URL to work with
         driver.manage().window().maximize(); //maximize browser's window
 
@@ -61,38 +68,78 @@ public class PhotoDownload {
         checkForAd();
         WebElement gallery = waitUntil(presenceOfElementLocated(By.linkText("Gallery")));
         gallery.click();
-
         checkForAd();
 
         List<WebElement> photo_links = driver.findElements(By.xpath("//table[@id=\"thumbs-table\"]/tbody/tr/td/a[not(contains(@href,'EXIF'))][not(./img)]"));
-        System.out.println("zzzz" + photo_links.size());
+
+        ArrayList<String> phtot_links_urls = new ArrayList<String>();
+
+        for (int y=0; y<photo_links.size(); y++) {
+            String a = photo_links.get(y).getAttribute("href");
+            phtot_links_urls.add(a);
+        }
+
 
         for (int i = 0; i < photo_links.size(); i++) {
-            System.out.println("wwww " + photo_links.get(i).getAttribute("href"));
-            WebElement link = photo_links.get(i);
-            link.click();
-
+            driver.get(phtot_links_urls.get(i));
             try {
                 driver.findElement(By.linkText("Full Size Image")).click();
-                WebElement a = driver.findElement(By.xpath("//img[contains(@src,'FULLRES')]"));
-                Actions action = new Actions(driver);
-                action.moveToElement(a).perform();
-                action.contextClick(a).perform();
-                action.sendKeys("v").perform();
+                WebElement link_element = driver.findElement(By.xpath("//img[contains(@src,'FULLRES')]"));
 
-                Robot robot = new Robot();
+                String link1 = link_element.getAttribute("src");
+                System.out.println(link1);
 
-                    robot.keyPress(KeyEvent.VK_ENTER);
-                    robot.keyRelease(KeyEvent.VK_ENTER);
-                    robot.delay(2000);
+                URL url = new URL(link1);
+                String file_name = url.getFile();
+                String destination = jpg_directory + file_name.substring(file_name.lastIndexOf("/"));
+
+                System.out.println(destination);
+
+                InputStream is = url.openStream();
+                OutputStream os = new FileOutputStream(destination);
+
+                byte[] bt = new byte[2048];
+                int length;
+
+                while ((length = is.read(bt)) != -1) {
+                    os.write(bt,0,length);
+                }
+
+                is.close();
+                os.close();
+
+            } catch (NoSuchElementException e) {
+                System.out.println(e);
+            }
+
+            try {
+                WebElement b = driver.findElement(By.xpath("//a[@class='caption']"));
+                String c = b.getAttribute("href");
+                URL url = new URL(c);
+                String file_name = url.getFile();
+                String destination = raw_directory + file_name.substring(file_name.lastIndexOf("/"));
+
+                System.out.println(destination);
+
+                InputStream is = url.openStream();
+                OutputStream os = new FileOutputStream(destination);
+
+                byte[] bt = new byte[2048];
+                int length;
+
+                while ((length = is.read(bt)) != -1) {
+                    os.write(bt,0,length);
+                }
+
+                is.close();
+                os.close();
+
+                System.out.println("success");
 
             } catch (NoSuchElementException e) {
                 System.out.println(e);
             }
         }
-
-        //FileUtils.cleanDirectory(photo_folder);
-
     }
 
     @AfterMethod
