72c3071064a26b877e3e4442d30b00fa677a2125
Ira <ira_www@yahoo.com>
2016-10-18 10:52:29 +0300

    downloadImage method accepts params, check if folder is empty, check if file was fully downloaded

--- src/test/java/Download.java
+++ src/test/java/Download.java
@@ -1,5 +1,6 @@
 
 import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
@@ -8,30 +9,55 @@ import java.net.URL;
 public class Download {
     static String directory = "/Users/Ira/Pictures/2/";
     static String image_url = "https://cdn.shopify.com/s/files/1/0051/4802/products/productpic2atommug_1024x1024.jpg?v=1448907134";
+    static String destination;
 
-    public static void downloadImage() throws IOException {
-        new File(directory).mkdir();
+    public static void downloadImage(String directory, String image_url) throws IOException {
+        File file_object = new File(directory);
 
-        URL url = new URL(image_url);
-        String file = url.getFile();
-        String destination = directory + file.substring(file.lastIndexOf("/"), file.indexOf("?"));
+        if(file_object.list().length>1) {
+            System.out.println("you can't use this folder");
+        }else {
+            System.out.println("go ahead");
+            new File(directory).mkdir();
 
-        InputStream inputStream = url.openStream();
-        OutputStream outputStream = new FileOutputStream(destination);
+            URL url = new URL(image_url);
+            String file = url.getFile();
 
-        byte[] bt = new byte[2048];
-        int length;
+            destination = directory + file.substring(file.lastIndexOf("/"), file.indexOf("?"));
 
-        while ((length = inputStream.read(bt)) != -1) {
-            outputStream.write(bt,0,length);
+            InputStream inputStream = url.openStream();
+            OutputStream outputStream = new FileOutputStream(destination);
+
+            byte[] bt = new byte[2048];
+            int length;
+
+            while ((length = inputStream.read(bt)) != -1) {
+                outputStream.write(bt, 0, length);
+            }
+
+            inputStream.close();
+            outputStream.close();
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("HEAD");
+            double file_size_online = (double) connection.getContentLengthLong();
+            System.out.println("Size in the wab " + file_size_online);
+
+
+            File file1 = new File("/Users/Ira/Pictures/2/productpic2atommug_1024x1024.jpg");
+            file1.exists();
+            double file_size_in_folder = file1.length();
+            System.out.println("File size " + file_size_in_folder);
+
+            if (file_size_in_folder == file_size_online) {
+                System.out.println("File is fully downloaded");
+            } else System.out.println("File is corrupted");
         }
 
-        inputStream.close();
-        outputStream.close();
     }
 
     public static void main(String[] args) throws IOException {
-        downloadImage();
+        downloadImage(directory, image_url);
         System.out.println("DONE");
     }
 }
